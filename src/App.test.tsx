918d278857f8169f4e61e6682d4197d0248b4570
jsapro <77.3.77@mail.ru>
2023-11-04 17:36:41 +0300

    fix(App.test.tsx):fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-	const { getByText } = render(
+test('renders "ToDo List" title', () => {
+	render(
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	);
 
-	expect(getByText(/learn/i)).toBeInTheDocument();
+	expect(screen.getByText(/ToDo List/i)).toBeInTheDocument();
 });
