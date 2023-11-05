854f8576e0f3eac9216cb1f565a5fa8990ed260e
jsapro <77.3.77@mail.ru>
2023-11-05 18:01:35 +0300

    fix(App.test/tsx): fix error with type

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../../src/store/store';
 import App from './App';
