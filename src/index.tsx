cdf04b3118e37743392f0e3255d6c292ee41dd4c
jsapro <77.3.77@mail.ru>
2023-11-06 18:54:16 +0300

    fix(index.tsx): small improvments

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,14 @@ import { Provider } from 'react-redux';
 /* APPLICATION */
 import { store } from './store/store';
 import App from './components/App/App';
-//import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import 'normalize.css';
 import './index.css';
 
+reportWebVitals(console.log);
+
 const container = document.getElementById('root');
-if (container !== null) {
+if (container) {
 	const root = createRoot(container);
 	root.render(
 		<React.StrictMode>
@@ -24,8 +26,3 @@ if (container !== null) {
 		</React.StrictMode>
 	);
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
