08c554f9b197ff415dcc7c86ab0f7d184be747ec
jsapro <77.3.77@mail.ru>
2023-11-06 15:27:10 +0300

    fix: delete unused comments

--- src/components/Modal/ModalInput.tsx
+++ src/components/Modal/ModalInput.tsx
@@ -8,7 +8,6 @@ import type { ModalInputProps } from './types';
 
 export const ModalInput: React.FC<ModalInputProps> = ({ name, setName, size }) => {
 	return (
-		// <div className={size === 'large' ? 'modalinput-wrapper large' : 'modalinput-wrapper'}>
 		<div className={clsx('modalinput-wrapper', { large: size === 'large' })}>
 			<input
 				id="modalinput"
