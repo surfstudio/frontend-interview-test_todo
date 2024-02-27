afbc648f5ec671daf93a567fb4c7aaa795cfa94e
timothyman1 <av.sedov42@gmail.com>
2024-02-27 14:53:02 +0300

    fix: eslint test error

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import App from './App';
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(screen.getByText(/learn/i)).toBeInTheDocument();
 });
